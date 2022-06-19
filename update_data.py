6238124200d0634a5fdef1ee79387f4c8d38af15
Anders Stig Kjærgaard 201607577@post.au.dk <au567859@s08n24.prime.net>
2022-06-19 16:07:41 +0200

    Added padding-network

--- update_data.py
+++ update_data.py
@@ -3,7 +3,7 @@
 #%%
 from DataHandling.data_raw.make_dataset import append_tozarr
 
-
-a=append_tozarr()
-
+store="/home/au567859/DataHandling/data_test/interim/data.zarr"
+raw = "/home/au567859/DataHandling/data_test/raw/"
+a=append_tozarr(store,raw)
 
